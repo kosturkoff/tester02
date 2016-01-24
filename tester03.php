64084189af6ffc2e471899ce86e9a176875f0fb6
kosturkoff <kosturkoff@abv.bg>
2016-01-24 17:03:35 +0200

    test

---
+++ tester03.php
@@ -0,0 +1,28 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+	<style type="text/css">
+	#frst {
+		background-color: red;
+	}
+	#scnd {
+		background-color: green;
+	}
+	#thrd {
+		background-color: blue;
+	}
+	</style>
+</head>
+<body>
+	<?php 
+
+$first="name";
+$second="222";
+$third="hguwe737839uwhfhgy67";
+
+echo "<p id='frst'>".$first."</p><p id='scnd'>".$second."</p><p id='thrd'>".$third."</p>"
+
+?>
+</body>
+</html>
